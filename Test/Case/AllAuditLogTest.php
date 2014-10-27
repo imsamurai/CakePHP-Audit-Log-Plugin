5b93445fef9f438690d943ea5ca108a8fa88812a
imsamurai <imsamurai@imsamurai-2>
2014-10-27 10:33:38 +0200

    add all tests file, travis, licanse, misc files, change composer #1

---
+++ Test/Case/AllAuditLogTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Author: imsamurai <im.samuray@gmail.com>
+ * Date: 27.10.2014
+ * Time: 10:30:00
+ */
+
+/**
+ * All AuditLog test suite
+ * 
+ * @package AllAuditTest
+ * @subpackage Test
+ */
+class AllAuditLogTest extends PHPUnit_Framework_TestSuite {
+
+	/**
+	 * Suite define the tests for this suite
+	 *
+	 * @return void
+	 */
+	public static function suite() {
+		$suite = new CakeTestSuite('All AuditLog Tests');
+
+		$path = App::pluginPath('AuditLog') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+		return $suite;
+	}
+
+}
